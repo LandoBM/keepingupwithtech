275bdc26a4c119eb4c0bd44504971c656e558b53
Landon Murray <landonbmurray@icloud.com>
2023-02-26 15:52:01 -0500

    trying to fixt fetch for my 'add comment button' that is currently not working properly and adding the comment to the post handlebar

--- public/js/addcom.js
+++ public/js/addcom.js
@@ -1,13 +1,13 @@
 const addCommentHandler = async (e) => {
     e.preventDefault()
     const id = document.querySelector('.btn').getAttribute('data-id')
-    const comment = document.querySelector('.commentBody').value.trim()
+    const comment_text = document.querySelector('.commentBody').value.trim()
     // const id = document.querySelector('.btn').getAttribute('data-id')
 
-    if (comment) {
-        const response = await fetch (`/api/comments/${id}`, {
+    if (comment_text) {
+        const response = await fetch (`/api/comments`, {
             method: 'POST',
-            body: JSON.stringify({comment_text: comment, user: id}),
+            body: JSON.stringify({comment_text}),
             headers: {
                 'Context-Type': 'application/json'
             },
